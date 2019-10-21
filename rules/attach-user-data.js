469db2e398ad4e1e35e1b6b66496c714b1f6af63
Jared Cuffe <jcuffe@gmail.com>
2019-10-21 10:51:33 -0700

    Attaches region field to user profile.
    Co-authored-by: Asa Shalom <asaofdiamonds@gmail.com>
    Co-authored-by: Michelle Okagbue <umbralcompendium@gmail.com>

--- rules/attach-user-data.js
+++ rules/attach-user-data.js
@@ -1,4 +1,6 @@
 function attachUserData(user, context, callback) {
+  const { zipLookup } = configuration
+  user.user_metadata.region = zipLookup[user.user_metadata.zip]
   context.idToken.profile = user
   callback(null, user, context)
 }
